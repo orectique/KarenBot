bbffdb868d0da0abbcd0a7d2f1d70e6be8a7cdbd
Sreehari Pulickamadhom Sreedhar <49713741+orectique@users.noreply.github.com>
2021-08-09 16:07:20 +0000

    added -roll/skip and previleges

--- main.py
+++ main.py
@@ -10,62 +10,94 @@ async def on_ready():
     print('Bot is ready')
 
 Story = ''
-
+Count = []
 Queue = []
-Que = []
 
 @client.event
 async def on_message(message):
     global Queue
     global Story
+    global Count
+    global Mod
 
     if message.author == client.user:
         return
 
-    if message.content.startswith('$karen-me'):
-        name = message.author.name
-        Queue.append(message.author)
-        tag = str(name) + ' has been added to the list!'
-        await message.channel.send(tag)
+    if message.content.startswith('-me'):
+        if len(Queue) == 0 or message.author != Queue[-1]:
+            name = message.author.name
+            Queue.append(message.author)
+            tag = str(name) + ' has been added to the queue'
+            await message.channel.send(tag)
+        else:
+            await message.channel.send('You cannot reserve consecutive spots.')
     
-    if message.content.startswith('$karen-read'):
+    if message.content.startswith('-read'):
         if len(Queue) != 0:
             if message.author == Queue[0]:
                 passage = message.content
-                passage = passage[11:]
+                if passage[5] == '':
+                    passage = passage[6:]
+                else:
+                    passage = passage[5:]
                 Story = Story + '\n' + passage
                 await message.channel.send('Okay, I got that!')
-                await message.channel.send(Story)
-#have to fix the author-queue lock.
+
                 if len(Queue) == 1:
-                   await message.channel.send('That\'s all for the day.')
+                    Queue = []
+                    await message.channel.send('That\'s all for the day.')
                 else:
                     Queue = Queue[1:]
                     tag = str(Queue[0].name) + ', you\'re next'
                     await message.channel.send(tag)
-        
-        else:
-            await message.channel.send('Where\'s everybody gone to?')
+            else:
+                tag = str(message.author.name) + ', please wait for your turn.'
+                await message.channel.send(tag)
 
-    if message.content.startswith('$karen-narrate'):
+        else:
+            await message.channel.send('Please join the queue first.')
+
+    if message.content.startswith('-narrate'):
         if len(Story) == 0:
             await message.channel.send('You haven\'t written anything yet!')
         else:     
             await message.channel.send(Story)
 
-    if message.content.startswith('$karen-help'):
+    if message.content.startswith('-help'):
         await message.channel.send('Where\'s your manager? You work for me; I don’t work for you.')
     
-    if message.content.startswith('$karen-skip'):
-        tag = str(Queue[0].name) + ' was skipped.'
-        await message.channel.send(tag)
-        Queue = Queue[1:]
-        tag = str(Queue[0].name) + ', you\'re next'
-        await message.channel.send(tag)
+    if message.content.startswith('-skip'):
+        if message.author.guild_permissions.manage_messages == True or message.author == Queue[0]:
+            if len(Queue) != 0:
+                tag = str(Queue[0].name) + ' was skipped.'
+                await message.channel.send(tag)
+                Queue = Queue[1:]
+                tag = str(Queue[0].name) + ', you\'re next'
+                await message.channel.send(tag)
+
+            else:
+                await message.channel.send('Nobody in the queue.')
         
-    if message.content.startswith('$karen-reset'):
-        Story = ''
-        Queue = []
+        else:
+            await message.channel.send('You cannot skip positions on the queue.')
+
+    if message.content.startswith('-reset'):
+        if message.author.guild_permissions.manage_messages == True:  
+            Story = ''
+            Queue = []
+            await message.channel.send('KarenBot has been reset.')
+        else:
+            await message.channel.send('You do not have the authority to reset KarenBot.')
+
+    if message.content.startswith('-queue'):
+        tag = 'Members in queue:'
+        for member in Queue:
+            tag += '\n\t' + str(member.name)
+        
+        await message.channel.send(tag) 
+
+    if message.content.startswith('-roll'):
+        await message.channel.send('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
 TOKEN = os.getenv("TOKEN")
 client.run(TOKEN)
